41ef16a1de8f53df11c019aa22648e391cedbdc0
kotankota <kotanakajimabayern20021022@gmail.com>
2023-05-24 00:37:07 +0900

    .npyファイルに出力するように変更

--- util/detect_replay.py
+++ util/detect_replay.py
@@ -10,14 +10,21 @@ def natural_keys(text):
     return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
 
-
+#txtファイル名からフレームの番号取得して(~~~_123.txtなら123)ndarrayに入れていく形にしています。
 def get_max_bbox_size(file_path,totalframecount):
     label_path = file_path + "/*.txt"
     max_bbox_size_array = np.zeros(totalframecount)
-    
+    movie_name = ""
+
     for file in sorted(glob.iglob(label_path),key = natural_keys):
         max_bbox_size = 0
         with open(file) as f:
+            #.npyの命名用
+            file_parsed = file.split('/')[-1]
+            pattern = r"(.+)_\d+\.txt"
+            movie_name = re.search(pattern, file_parsed).group(1)      
+            
+            
             frame = int(file.split('_')[-1].split('.txt')[0])
             for line in f:
                 xywh = line.split(' ')
@@ -29,6 +36,10 @@ def get_max_bbox_size(file_path,totalframecount):
                         max_bbox_size = float_xywh[2] * float_xywh[3] * 100
             
             max_bbox_size_array[frame - 1] = max_bbox_size
+    
+    npy_name = "max_bbox_size_" + movie_name + ".npy"
+    np.save(npy_name, max_bbox_size_array)
+
     return max_bbox_size_array
 
 
@@ -41,13 +52,12 @@ def get_zoom_frames(max_bbox_size_array,threshold=1):
         else:
             zoom_frames[i] = 0
     return zoom_frames
-
     
                 
 def main():
     #example
-    totalframecount = 1188
-    file_path = "/Users/kota/Desktop/soccer_net/labels_replay"
+    totalframecount = 69127
+    file_path = "labels"
     threshold = 1
     
     max_bbox_size_array = get_max_bbox_size(file_path,totalframecount)
